897a32cfca20a889f31bd330212405befc327c11
ashk9625-lab <ashk9625@gmail.com>
2026-08-20 15:43:05 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='baked-image-viewer-v4';
+const CACHE='baked-correct-admin-surprise-strain-v1';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','baked-logo.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]));});
